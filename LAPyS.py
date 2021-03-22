0da6763a3cc0e92257ee0f61272f410378270379
ZSVS <stepanezc@gmail.com>
2021-03-22 16:14:16 +0200

    Fix bug with GlobalProtect ICMP reject. Added default server address to return, if no PING reply

--- LAPyS.py
+++ LAPyS.py
@@ -17,24 +17,31 @@ SERVERS_POOL = {
         "DC-HPI-01" : "10.1.249.118"
         }
 
-def GetOptimalServer(ServersPool): 
+def GetOptimalServer(ServersPool):
     """
         Get servers from dictionary(Server Name -> IPv4 Address). 
         It will check delay time of all servers and return address of best one 
     """
     ServerDelayDict = dict()
+    DefServerAddr = "10.1.249.118"
     PivotTime = 1.0
     OptimalServerAddr = ""
     for key in ServersPool.keys():
         ServerDelayDict[key] = ping(ServersPool[key])
         #! Add for debug info print(key," - ",ping(ServersPool[key]))
         for DelayTime in ServerDelayDict.values():
-                if DelayTime <= PivotTime:
-                        PivotTime = DelayTime
-                        OptimalServerAddr = ServersPool[key]
-                        OptimalServerName = key
-    WriteToLog("Pinging server pool. Get {0} as main DC".format(OptimalServerName))
-    return OptimalServerAddr
+                try:
+                    if DelayTime <= PivotTime:
+                            PivotTime = DelayTime
+                            OptimalServerAddr = ServersPool[key]
+                            OptimalServerName = key
+                except TypeError:
+                    WriteToLog("No ICMP answer from servers")
+                    WriteToLog("Function return default server {0}".format(DefServerAddr))
+                    return DefServerAddr
+                else:
+                    WriteToLog("Pinging server pool. Get {0} as main DC".format(OptimalServerName))
+                    return OptimalServerAddr
         
 def CheckNetwork():
     try:
@@ -145,11 +152,13 @@ def GetPassword(Event):
     global SERVERS_POOL
     if CheckUserCred_NotNullOrEmpty(TextBoxUserContext.get(), TextBoxPasswordContext.get()):
         Net = CheckNetwork()
+       
+        if Net == socket.herror:
+            return None
 
         OptimalServer = GetOptimalServer(SERVERS_POOL)
 
-        if Net == socket.herror:
-            return None
+
 
         if STATE_FLAG:
             WriteToLog("Uses credential from file")
