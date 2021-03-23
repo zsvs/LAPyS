a683d40b27fa54edc19da23cb1db1d0ebf68a82a
ZSVS <stepanezc@gmail.com>
2021-03-23 13:59:39 +0200

    Decomposition

---
+++ Improved/Network/Network_core.py
@@ -0,0 +1,43 @@
+import socket
+from ping3 import ping, verbose_ping
+import LAPyS.Logging.LAPyS_Logging as Log
+import LAPyS.Network.Servers_pool as SRV_POOL
+
+
+def GetOptimalServer(ServersPool):
+    """
+        Get servers from dictionary(Server Name -> IPv4 Address). 
+        It will check delay time of all servers and return address of best one 
+    """
+    ServerDelayDict = dict()
+    DefServerAddr = "10.1.249.118"
+    PivotTime = 1.0
+    OptimalServerAddr = ""
+    for key in ServersPool.keys():
+        ServerDelayDict[key] = ping(ServersPool[key])
+        #! Add for debug info print(key," - ",ping(ServersPool[key]))
+        for DelayTime in ServerDelayDict.values():
+                try:
+                    if DelayTime <= PivotTime:
+                            PivotTime = DelayTime
+                            OptimalServerAddr = ServersPool[key]
+                            OptimalServerName = key
+                except TypeError:
+                    Log.WriteToLog("No ICMP answer from servers")
+                    Log.WriteToLog("Function return default server {0}".format(DefServerAddr))
+                    return DefServerAddr
+                else:
+                    Log.WriteToLog("Pinging server pool. Get {0} as main DC".format(OptimalServerName))
+                    return OptimalServerAddr
+        
+def CheckNetwork():
+    try:
+        Log.WriteToLog("Network test started")
+        hostname, domain = socket.gethostbyaddr(SRV_POOL.SERVERS_POOL["DC-KV-01"])[0].partition('.')[::2]
+    except socket.herror:     
+        Log.WriteToLog("No connection to domain network")
+        #!TextBoxDomainComputerRML.insert(0, "No connection to domain network")
+        #print(socket.herror)
+        return socket.herror
+    else:
+        Log.WriteToLog("Successfully connected to {0}".format(hostname + "." + domain))
---
+++ Improved/UI/MainForm.py
@@ -0,0 +1,49 @@
+import tkinter as tk
+#import LAPyS.LAPyS_Core as Events
+from LAPyS.LAPyS_Core import Save, Load, GetPassword
+import LAPyS.Logging.LAPyS_Logging as Log
+
+window = tk.Tk()
+
+window.geometry('400x180')
+window.resizable(False, False)
+window.title("LAPyS")
+
+labelUserLogin = tk.Label(text="Enter your domain login:")
+labelUserLogin.place(x = 100, y = 5, width = 250, height = 15)
+
+TextBoxUserContext = tk.Entry()
+TextBoxUserContext.place(x = 100, y = 25, width = 250, height = 20)
+
+labelUserPassword = tk.Label(text="Enter your password:")
+labelUserPassword.place(x = 100, y = 45, width = 250, height = 20)
+
+TextBoxPasswordContext = tk.Entry()
+TextBoxPasswordContext["show"] = "*"
+TextBoxPasswordContext.place(x = 100, y = 65, width = 250, height = 20)
+
+labelRequestedName = tk.Label(text="Enter name of computer:")
+labelRequestedName.place(x = 100, y = 85, width = 250, height = 20)
+
+TextBoxDomainComputerName = tk.Entry()
+TextBoxDomainComputerName.place(x = 100, y = 105, width = 250, height = 20)
+
+TextBoxDomainComputerRML = tk.Entry()
+TextBoxDomainComputerRML.place(x = 100, y = 145, width = 250, height = 20)
+
+BtnLoad = tk.Button(window, text="Load", width=10, height=2, bg="white", fg="black")
+BtnLoad.bind("<Button-1>", Load)
+BtnLoad.place(x = 10, y = 25, width = 80, height = 20)
+
+BtnSave = tk.Button(window, text="Save", width=10, height=2, bg="white", fg="black")
+BtnSave.bind("<Button-1>", Save)
+BtnSave.place(x = 10, y = 65, width = 80, height = 20)
+
+BtnPassw = tk.Button(window, text="Get password", width=10, height=2, bg="white", fg="black")
+BtnPassw.bind("<Button-1>", GetPassword)
+BtnPassw.place(x = 10, y = 105, width = 80, height = 20)
+
+window.mainloop()
+
+if window.mainloop() == None:
+    Log.WriteToLog("Application killed!")
