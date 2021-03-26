48b66ce9703a9a04d13fa7f4019067665565fe30
ZSVS <stepanezc@gmail.com>
2021-03-26 15:46:46 +0200

    Added comments

--- UI/Network_Error.py
+++ UI/Network_Error.py
@@ -1,4 +1,6 @@
 from tkinter import messagebox as mb
-
+"""
+Show messagebox with error to user
+"""
 def NetError():
     mb.showerror(title="Error", message="No connection to domain network")
--- UI/UI_LDAP_Error.py
+++ UI/UI_LDAP_Error.py
@@ -1,6 +1,8 @@
 
 from tkinter import messagebox as mb
-
+"""
+Show messagebox with error to user
+"""
 #mb.askyesno(title="Error", message=)
 def LdapError():
     mb.showerror(title="Error", message="Invalid LDAP Credential")
