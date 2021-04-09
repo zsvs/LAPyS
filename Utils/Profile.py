70ed85fa1872f8c5171782d946eb9a31a6889a29
ZSVS <stepanezc@gmail.com>
2021-04-09 11:45:52 +0300

    Class for work with application directory

--- Utils/Profile.py
+++ Utils/Profile.py
@@ -4,27 +4,87 @@ import LAPyS.Logging.LAPyS_Logging as Log
 This module uses for create and check if profile is exist
 CREDNTIALS_PATH contains the full path to credentials file
 """
-__CREDNTIALS_PATH = os.getenv("USERPROFILE") + "\\LAPyS\\Credential.cred"
-__APP_PATH = os.getenv("USERPROFILE") + "\\LAPyS"
 
-try:
-    try:
-        Log.WriteToLog("Start checking profile directory")
-    except FileNotFoundError:
-        os.mkdir(os.getenv("USERPROFILE") + "\\LAPyS")
-        Log.WriteToLog("Profile directory created")
-    else:
-        Log.WriteToLog("Profile directory already exists")
-except FileExistsError:
-    Log.WriteToLog("Profile directory already exists")
+Logs = Log.Logger().GetInstance()
 
-try:
-    Log.WriteToLog("Check if credential file is exist")
-    f = open(__CREDNTIALS_PATH, "r")
-    f.close
-    Log.WriteToLog("Credential file is exist")
-except FileNotFoundError:
-    f = open(__CREDNTIALS_PATH, "w")
-    Log.WriteToLog("Credential file created")
-    f.close
+class AplicationProfile:
+    __CREDNTIALS_PATH = os.getenv("USERPROFILE") + "\\LAPyS\\Credential.cred"
+    __APP_PATH = os.getenv("USERPROFILE") + "\\LAPyS"
+
+    def __init__(self):
+        """
+        ctor check if exist profile dir 
+        and credentials file. If not then create them.
+        """
+        if not self.CheckProfileDirectoryExist():
+            self.__CreateProfileDirectory()
+        else:
+            Logs.WriteToLog("Profile directory already exists")
+
+        if not self.CheckCredentialFileExist():
+            self.__CreateCredentialFile()
+        else:
+            Logs.WriteToLog("Credential file is exist")
+
+    def __CreateProfileDirectory(self):
+        """
+        Creates profile directory
+        """
+        os.mkdir(self.__APP_PATH)
+        Logs.WriteToLog("Profile directory created")
+        
+    def CheckProfileDirectoryExist(self):
+        """
+        Return <True> if profile dir exist
+        """
+        return os.path.exists(self.__APP_PATH)
+
+    def __CreateCredentialFile(self):
+        """
+        Creates credentials file
+        """
+        f = open(self.__CREDNTIALS_PATH, "w")
+        f.close
+        Logs.WriteToLog("Credential file created")
+    
+    def CheckCredentialFileExist(self):
+        """
+        Return <True> if credentials file exist
+        """
+        return os.path.exists(self.__CREDNTIALS_PATH)
+        
+    def GetCredentialPath(self):
+        """
+        Return full path to credentials file
+        """
+        return self.__CREDNTIALS_PATH
+
+    def GetProfilePath(self):
+        """
+        Return full path to profile dir
+        """
+        return self.__APP_PATH
+
+Profile = AplicationProfile()
+
+#try:
+#    try:
+#        Logs.WriteToLog("Start checking profile directory")
+#    except FileNotFoundError:
+#        os.mkdir(os.getenv("USERPROFILE") + "\\LAPyS")
+#        Logs.WriteToLog("Profile directory created")
+#    else:
+#        Logs.WriteToLog("Profile directory already exists")
+#except FileExistsError:
+#    Logs.WriteToLog("Profile directory already exists")
+#
+#try:
+#    Logs.WriteToLog("Check if credential file is exist")
+#    f = open(__CREDNTIALS_PATH, "r")
+#    f.close
+#    Logs.WriteToLog("Credential file is exist")
+#except FileNotFoundError:
+#    f = open(__CREDNTIALS_PATH, "w")
+#    Logs.WriteToLog("Credential file created")
+#    f.close
 
