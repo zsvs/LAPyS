67e055543d469cab927103a9c36247d90206bf1c
ZSVS <stepanezc@gmail.com>
2021-04-09 11:48:24 +0300

    Minor decomposition in functions

--- LAPyS_Core.py
+++ LAPyS_Core.py
@@ -1,6 +1,6 @@
 import os
 import LAPyS.UI.MainForm as Form
-import LAPyS.Utils.Profile as Profile
+from LAPyS.Utils.Profile import Profile
 import LAPyS.Logging.LAPyS_Logging as Log
 import LAPyS.Network.Network_core as NetCore
 import LAPyS.Network.LDAP_Connect as ldap
@@ -9,39 +9,31 @@ import LAPyS.Encryption.Encryption as Encr
 import LAPyS.JSON_Classes.Marshaling as Marsh
 
 STATE_FLAG = False
+Logs = Log.Logger().GetInstance()
 
 def Load(Event):
     global STATE_FLAG
     STATE_FLAG = True
-    Form.TextBoxUserContext.delete(0, "end")
-    Form.TextBoxPasswordContext.delete(0, "end")
-    Form.BtnSave.configure(state="disabled")
-    Form.BtnLoad.configure(state="disabled")
-    #FILE_PATH = #os.getenv("USERPROFILE") + "\\LAPyS Log\\Credential.cred"
-    with open(Profile.__CREDNTIALS_PATH, "r") as Cred:
+    with open(Profile.GetCredentialPath(), "r") as Cred:
         DECODED_NAME = Cred.readlines(1)
         DECODED_PASSW = Cred.readlines(1)
         Form.TextBoxUserContext.insert(0, Encr.Decrypt(DECODED_NAME[0].split(",")))
         Form.TextBoxPasswordContext.insert(0, Encr.Decrypt(DECODED_PASSW[0].split(",")))
-        Log.WriteToLog("Profile loaded from file Credential.cred -> {0}".format(STATE_FLAG))
-    Form.TextBoxUserContext.configure(state="disabled")
-    Form.TextBoxPasswordContext.configure(state="disabled")
+        Logs.WriteToLog("Profile loaded from file Credential.cred -> {0}".format(STATE_FLAG))
+    Form.OnLoadPressed()
 
 def Save(Event):
     global STATE_FLAG
     if UserCred.NotNullOrEmpty(Form.TextBoxUserContext.get(), Form.TextBoxPasswordContext.get()) and UserCred.isAdministrator(Form.TextBoxUserContext.get()):
         STATE_FLAG = True
-        Form.BtnLoad.configure(state="disabled")
-        #FILE_PATH = CREDNTIALS_PATH os.getenv("USERPROFILE") + "\\LAPyS Log\\Credential.cred"
-        with open(Profile.__CREDNTIALS_PATH, "w") as Cred:
+        with open(Profile.GetCredentialPath(), "w") as Cred:
             ENCODED_NAME = Encr.Encrypt(Form.TextBoxUserContext.get())
             ENCODED_PASSW = Encr.Encrypt(Form.TextBoxPasswordContext.get())
             Cred.write(str(ENCODED_NAME)[1:len(str(ENCODED_NAME))-1] + "\n" + str(ENCODED_PASSW)[1:len(str(ENCODED_PASSW))-1])
-            Log.WriteToLog("Profile saved from entry fields -> {0}".format(STATE_FLAG))
-        Form.TextBoxUserContext.configure(state="disabled")
-        Form.TextBoxPasswordContext.configure(state="disabled")
+            Logs.WriteToLog("Profile saved from entry fields -> {0}".format(STATE_FLAG))
+        Form.OnSavePressed()
     else: 
-        Log.WriteToLog("Operation Save failed. Empty user credential fields.")
+        Logs.WriteToLog("Operation Save failed. Empty user credential fields.")
         Form.TextBoxUserContext.insert(0,"Please enter your login and password")
 
 def GetPassword(Event):
@@ -51,14 +43,13 @@ def GetPassword(Event):
         if Net == NetCore.socket.herror:
             return None
         JSON = Marsh.Marshaling().GetInstance()
-        SRV = JSON.Deserialize(Profile.__APP_PATH +  "\\SERVERS_POOL.json")
+        SRV = JSON.Deserialize(Profile.GetProfilePath() +  "\\SERVERS_POOL.json")
         OptimalServer = NetCore.GetOptimalServer(SRV)
 
         if STATE_FLAG:
-            Log.WriteToLog("Uses credential from file")
+            Logs.WriteToLog("Uses credential from file")
         else:
-            Log.WriteToLog("Uses entry fields credentials")
-            
+            Logs.WriteToLog("Uses entry fields credentials")
 
         Form.TextBoxDomainComputerRML.delete(0, "end")
         UserContextLocal = Form.TextBoxUserContext.get()
@@ -68,15 +59,15 @@ def GetPassword(Event):
             Form.TextBoxUserContext.delete(0, "end")
             Form.TextBoxPasswordContext.delete(0, "end")
             Form.TextBoxUserContext.insert(0, "{0} is not administrator!".format(UserContextLocal))
-            Log.WriteToLog("{0} is not administrator!".format(UserContextLocal))
+            Logs.WriteToLog("{0} is not administrator!".format(UserContextLocal))
             return None
 
         RequestedNameLocal = Form.TextBoxDomainComputerName.get()
 
         if len(RequestedNameLocal) == 0:
-            Log.WriteToLog("Requested name is empty!")
+            Logs.WriteToLog("Requested name is empty!")
         else:
-            Log.WriteToLog("Requested name -> {0}".format(RequestedNameLocal))
+            Logs.WriteToLog("Requested name -> {0}".format(RequestedNameLocal))
 
         AD = ldap.get_ldap_info(UserContextLocal, PasswordContextLocal, RequestedNameLocal, OptimalServer)
         AD_Computers = dict() #TODO Think about creates global dict to store all results for improving speed of searching.
@@ -87,11 +78,11 @@ def GetPassword(Event):
             Form.TextBoxDomainComputerRML.insert(0, AD_Computers[RequestedNameLocal])
         except KeyError:
             Form.TextBoxDomainComputerRML.insert(0, "No such name in OU")
-            Log.WriteToLog("No such name in OU")
-            Log.WriteToLog("Connection to LDAP server closed")
+            Logs.WriteToLog("No such name in OU")
+            Logs.WriteToLog("Connection to LDAP server closed")
         else:
-            Log.WriteToLog("RML successfully returned")
-            Log.WriteToLog("Connection to LDAP server closed")
+            Logs.WriteToLog("RML successfully returned")
+            Logs.WriteToLog("Connection to LDAP server closed")
     else:
         Form.TextBoxUserContext.insert(0,"Please enter your login and password")
         Form.TextBoxDomainComputerRML.insert(0,"Please enter your login and password")
