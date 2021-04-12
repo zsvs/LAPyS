bf5bf13ab6311c4ab0486e35b13ec9d4b237924d
ZSVS <stepanezc@gmail.com>
2021-04-12 14:40:40 +0300

    Using UserClass instance. Reworked imports.

--- LAPyS_Core.py
+++ LAPyS_Core.py
@@ -1,48 +1,48 @@
 import os
 import LAPyS.UI.MainForm as Form
-from LAPyS.Utils.Profile import Profile
-import LAPyS.Logging.LAPyS_Logging as Log
 import LAPyS.Network.Network_core as NetCore
 import LAPyS.Network.LDAP_Connect as ldap
-import LAPyS.Utils.CheckUserCred as UserCred
 import LAPyS.Encryption.Encryption as Encr
-import LAPyS.JSON_Classes.Marshaling as Marsh
+from LAPyS.Utils.Profile import Profile
+from LAPyS.Logging.LAPyS_Logging import Logs
+from LAPyS.JSON_Classes.Marshaling import JSON
+from LAPyS.UI.UserCreation_Error import Creation_Error
+import LAPyS.Utils.UserClass as UserClass
+
+User_OnLoad = None
+User_OnSave = None
+User_FromEntry = None
 
 STATE_FLAG = False
-Logs = Log.Logger().GetInstance()
 
 def Load(Event):
-    global STATE_FLAG
+    global STATE_FLAG, User_OnLoad
     STATE_FLAG = True
+    Form.onLoad_ClearFields()
     with open(Profile.GetCredentialPath(), "r") as Cred:
-        DECODED_NAME = Cred.readlines(1)
-        DECODED_PASSW = Cred.readlines(1)
-        Form.TextBoxUserContext.insert(0, Encr.Decrypt(DECODED_NAME[0].split(",")))
-        Form.TextBoxPasswordContext.insert(0, Encr.Decrypt(DECODED_PASSW[0].split(",")))
+        User_OnLoad = UserClass.User(ObjectName = "onLoad", Name = Encr.Decrypt((Cred.readlines(1))[0].split(",")), Password = Encr.Decrypt((Cred.readlines(1))[0].split(",")) )
+        Form.TextBoxUserContext.insert(0, User_OnLoad.GetName())
+        Form.TextBoxPasswordContext.insert(0, User_OnLoad.GetPassword())
         Logs.WriteToLog("Profile loaded from file Credential.cred -> {0}".format(STATE_FLAG))
-    Form.OnLoadPressed()
 
 def Save(Event):
-    global STATE_FLAG
-    if UserCred.NotNullOrEmpty(Form.TextBoxUserContext.get(), Form.TextBoxPasswordContext.get()) and UserCred.isAdministrator(Form.TextBoxUserContext.get()):
+    global STATE_FLAG, User_OnSave
+    if UserClass.User.NotNullOrEmpty(Form.TextBoxUserContext.get(), Form.TextBoxPasswordContext.get()) and UserClass.User.isAdministrator(Form.TextBoxUserContext.get()):
         STATE_FLAG = True
         with open(Profile.GetCredentialPath(), "w") as Cred:
-            ENCODED_NAME = Encr.Encrypt(Form.TextBoxUserContext.get())
-            ENCODED_PASSW = Encr.Encrypt(Form.TextBoxPasswordContext.get())
-            Cred.write(str(ENCODED_NAME)[1:len(str(ENCODED_NAME))-1] + "\n" + str(ENCODED_PASSW)[1:len(str(ENCODED_PASSW))-1])
+            User_OnSave = UserClass.User(ObjectName = "onSave", Name = Form.TextBoxUserContext.get(), Password = Form.TextBoxPasswordContext.get())
+            Cred.write(str(Encr.Encrypt(Form.TextBoxUserContext.get())) + "\n" + str(Encr.Encrypt(Form.TextBoxPasswordContext.get())))
             Logs.WriteToLog("Profile saved from entry fields -> {0}".format(STATE_FLAG))
-        Form.OnSavePressed()
     else: 
         Logs.WriteToLog("Operation Save failed. Empty user credential fields.")
         Form.TextBoxUserContext.insert(0,"Please enter your login and password")
 
 def GetPassword(Event):
-    if UserCred.NotNullOrEmpty(Form.TextBoxUserContext.get(), Form.TextBoxPasswordContext.get()):
+    if UserClass.User.NotNullOrEmpty(Form.TextBoxUserContext.get(), Form.TextBoxPasswordContext.get()):
         Net = NetCore.CheckNetwork()
        
         if Net == NetCore.socket.herror:
             return None
-        JSON = Marsh.Marshaling().GetInstance()
         SRV = JSON.Deserialize(Profile.GetProfilePath() +  "\\SERVERS_POOL.json")
         OptimalServer = NetCore.GetOptimalServer(SRV)
 
@@ -50,17 +50,15 @@ def GetPassword(Event):
             Logs.WriteToLog("Uses credential from file")
         else:
             Logs.WriteToLog("Uses entry fields credentials")
+            try:
+                User_FromEntry = UserClass.User(ObjectName = "FromEntryFields", Name = Form.TextBoxUserContext.get(), Password = Form.TextBoxPasswordContext.get())
+            except Exception:
+                Creation_Error(Form.TextBoxUserContext.get())
+                return None
 
         Form.TextBoxDomainComputerRML.delete(0, "end")
-        UserContextLocal = Form.TextBoxUserContext.get()
-        PasswordContextLocal = Form.TextBoxPasswordContext.get()
-        
-        if not UserContextLocal.startswith("sa"):
-            Form.TextBoxUserContext.delete(0, "end")
-            Form.TextBoxPasswordContext.delete(0, "end")
-            Form.TextBoxUserContext.insert(0, "{0} is not administrator!".format(UserContextLocal))
-            Logs.WriteToLog("{0} is not administrator!".format(UserContextLocal))
-            return None
+        UserContextLocal = User_FromEntry.GetName()
+        PasswordContextLocal = User_FromEntry.GetPassword() 
 
         RequestedNameLocal = Form.TextBoxDomainComputerName.get()
 
