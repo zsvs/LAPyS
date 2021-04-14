1adaa3abcf41def1c06ffdb31d0ccc58893b985b
ZSVS <stepanezc@gmail.com>
2021-04-14 15:08:32 +0300

    Rewrite to statc class

--- Encryption/Encryption.py
+++ Encryption/Encryption.py
@@ -1,14 +1,26 @@
 """
 Module provides simple functions for encoding/decoding in ASCII table
 """
-def Encrypt(String):
-    lst = list()
-    for symbol in String:
-        lst.append(ord(symbol))
-    return str(lst)[1:len(str(lst))-1]
+class Coder:
+    """
+    Static class for encode/decode data to/from ASCII
+    """
+    @staticmethod
+    def Encrypt(String):
+        """
+        Return string in ASCII representation, splitted by comma
+        """
+        lst = list()
+        for symbol in String:
+            lst.append(ord(symbol))
+        return str(lst)[1:len(str(lst))-1]
 
-def Decrypt(ByteList):
-    st = str()
-    for ASCII_Code in ByteList:
-        st += chr(int(ASCII_Code))
-    return st
+    @staticmethod
+    def Decrypt(ByteList):
+        """
+        Return string decoded from ASCII
+        """
+        st = str()
+        for ASCII_Code in ByteList:
+            st += chr(int(ASCII_Code))
+        return st
