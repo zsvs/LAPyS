deb4f8b5db010e6b861985e4b53c555a95b14df5
ZSVS <stepanezc@gmail.com>
2021-04-19 17:15:44 +0300

    Simple structure for app data

---
+++ JSON_Classes/DataTemplate.py
@@ -0,0 +1,21 @@
+"""
+Module provides simple class that store 
+data which must be posted to web-server
+"""
+from datetime import datetime as dt
+
+class ExchangeData:
+    __DataTemplate = None
+
+    def __init__(self, UserName, Password, RequestedName):
+        self.__DataTemplate = { 
+                                    "UserName" :  UserName,
+                                    "Password" : Password,
+                                    "RequestedName" : RequestedName,
+                                    "Date" :  str(dt.now())
+                                }
+
+    def GetDataToExchange(self):
+        return self.__DataTemplate
+
+
