6ba0682b4bde417d16731ab44dcf9aa0974f2666
ZSVS <stepanezc@gmail.com>
2021-04-19 17:16:28 +0300

    Class to work with web server

--- Network/HTTP_core.py
+++ Network/HTTP_core.py
@@ -1,28 +1,53 @@
+"""
+Class to work with web-server
+"""
+
 import requests
-from datetime import datetime as dt
-import LAPyS.JSON_Classes.Marshaling as Serializer
+from LAPyS.JSON_Classes.Marshaling import JSON # Get instance of Marshaling class
+from LAPyS.Logging.LAPyS_Logging import Logs # Get instance of Logger class
 
-JSONdata ={ #! Just simple example
-        "UserName" : "sa.stepanets",
-        "Password" : "SOME_UGLY_BASTARD_PASSWORD",
-        "RequestedName" : "SOME_NAME",
-        "Date" :  str(dt.now())
-    }
+class HTTPClient:
+    __Payload = None
+    __Headers = None
+    __URL ="http://10.6.0.229:65065/some/post"
+    __Answer = None
 
-Data = Serializer.Marshaling().GetInstance() # Create an instance of Marshaling class
-Payload = Data.Serialize(JSONdata)
+    def __init__(self):
+        self.__Headers = {  
+                            "Content-type": "application/json",
+                            "Accept": "*/*",
+                            "User-Agent" : "Python_Client",
+                            "Content-Encoding": "utf-8" 
+                        }
+    
+    def SetPayload(self, Data):
+        """
+        Setting up HTTP payload.
+        Mandatory attribute <Data>, preferred to be a dict()
+        """
+        self.__Payload = JSON.Serialize(Data)
 
-Headers = {"Content-type": "application/json",
-           "Accept": "*/*",
-           "User-Agent" : "Python_Client",
-           "Content-Encoding": "utf-8"}
-           #"Content-Length" : str(len(Payload))}
+    def GetPayload(self):
+        """
+        Getting active request payload
+        """
+        return self.__Payload
 
-URL = "https://sd.kernel.ua/plugins/servlet/theme/portal/2"
-#URLTEST = "http://192.168.1.41:65065/some/post"
-Answer = requests.post(URL, data = "0" , headers = Headers)
+    def PostRequest(self):
+        """
+        Sending POST request to web-server
+        """
+        if self.__Payload: 
+            self.__Answer = requests.post(self.__URL, data = self.__Payload, headers = self.__Headers)
+            Logs.WriteToLog("Data transited to web server")
+        else:
+            Logs.WriteToLog("No payload in HTTP request")
+            raise Exception("Payload must be setted")
+    
+    def GetAnswer(self):
+        """
+        Returns HTTP code of response
+        """
+        return self.__Answer
 
-print(Answer)
-print(Answer.headers)
-print(Answer.text)
-#print("Content-Length - ",len(Payload))
+WebClient = HTTPClient()
