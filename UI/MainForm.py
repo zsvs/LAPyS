76ddd788d2c9fabe500ffc5e627ce9d06ef37e9e
ZSVS <stepanezc@gmail.com>
2021-04-29 17:10:05 +0300

    Add new method for work with menu buttons

--- UI/MainForm.py
+++ UI/MainForm.py
@@ -8,7 +8,7 @@ import os
 from LAPyS.LAPyS_Core import Save, Load, GetPassword
 from LAPyS.Logging.LAPyS_Logging import Logs
 from LAPyS.Utils.Profile import Profile
-from LAPyS.UI.AddIP_Form import Ip_Add_Form
+from LAPyS.UI.AddIP_Form import AddIpForm
 
 window = tk.Tk()
 
@@ -19,33 +19,33 @@ window.title("LAPyS")
 mainMenu = tk.Menu(window)
 
 fileMenu = tk.Menu(mainMenu, tearoff=0)
-fileMenu.add_command(label = "Add ip to pool", command = Ip_Add_Form)
+fileMenu.add_command(label = "Add ip to pool", command = lambda: AddIpForm().GetInstance().Initialize())
 fileMenu.add_command(label = "View server pool", command = lambda: os.system(Profile.GetProfilePath() +  "\\SERVERS_POOL.json"))
 fileMenu.add_command(label = "Open logs", command = lambda: os.system(Logs.GetLogFilePath()))
 
 aboutMenu = tk.Menu(mainMenu, tearoff=0)
 aboutMenu.add_command(label = "About")
 
-labelUserLogin = tk.Label(text="Enter your domain login:")
+labelUserLogin = tk.Label(window, text="Enter your domain login:")
 labelUserLogin.place(x = 100, y = 25, width = 250, height = 15)
 
-TextBoxUserContext = tk.Entry()
+TextBoxUserContext = tk.Entry(window)
 TextBoxUserContext.place(x = 100, y = 45, width = 250, height = 20)
 
-labelUserPassword = tk.Label(text="Enter your password:")
+labelUserPassword = tk.Label(window, text="Enter your password:")
 labelUserPassword.place(x = 100, y = 65, width = 250, height = 20)
 
-TextBoxPasswordContext = tk.Entry()
+TextBoxPasswordContext = tk.Entry(window)
 TextBoxPasswordContext["show"] = "*"
 TextBoxPasswordContext.place(x = 100, y = 85, width = 250, height = 20)
 
-labelRequestedName = tk.Label(text="Enter name of computer:")
+labelRequestedName = tk.Label(window, text="Enter name of computer:")
 labelRequestedName.place(x = 100, y = 105, width = 250, height = 20)
 
-TextBoxDomainComputerName = tk.Entry()
+TextBoxDomainComputerName = tk.Entry(window)
 TextBoxDomainComputerName.place(x = 100, y = 125, width = 250, height = 20)
 
-TextBoxDomainComputerRML = tk.Entry()
+TextBoxDomainComputerRML = tk.Entry(window)
 TextBoxDomainComputerRML.place(x = 100, y = 165, width = 250, height = 20)
 
 BtnLoad = tk.Button(window, text="Load", width=10, height=2, bg="white", fg="black")
