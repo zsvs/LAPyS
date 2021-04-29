041c732f66ba5ca4e686f8830dbef6bf23d85c9d
ZSVS <stepanezc@gmail.com>
2021-04-29 17:09:25 +0300

    Class for create additional forms

--- UI/AddIP_Form.py
+++ UI/AddIP_Form.py
@@ -7,35 +7,62 @@ from LAPyS.Utils.Profile import Profile
 from LAPyS.JSON_Classes.Marshaling import JSON
 import os
 
-def Ip_Add_Form():
-    Ip_Add_Form_window = tk.Tk()
-    
-    def AddServer(Event):
+class AddIpForm:
+    __Instance = None
+    __EntryFieldsTextVariablesDict = dict()
+
+    def __init__(self):
+        if AddIpForm.__Instance:
+            print("Instance already created", self.GetInstance())
+        else:
+            print("No instance")
+
+    def AddServer(self, Event):
         Pool = JSON.Deserialize(Profile.GetProfilePath() +  "\\SERVERS_POOL.json")
-        Pool[TextBoxServerName.get()] = TextBoxServerIP.get()
+        Pool[self.__GetEntryText("ServerName")] = self.__GetEntryText("ServerIP")
         with open(Profile.GetProfilePath() +  "\\SERVERS_POOL.json", "w") as ServerPoolFile:
             ServerPoolFile.write(JSON.Serialize(Pool))
-            Logs.WriteToLog("Added " + TextBoxServerName.get() + " to servers pool")
+            Logs.WriteToLog("Added " + self.__GetEntryText("ServerName") + " to servers pool")
+        self.__ClearEntry("ServerName")
+        self.__ClearEntry("ServerIP")
+    
+    @classmethod
+    def GetInstance(cls):
+        if cls.__Instance == None:
+            cls.__Instance = AddIpForm()
 
-    Ip_Add_Form_window.geometry("160x110")
-    Ip_Add_Form_window.resizable(False, False)
-    Ip_Add_Form_window.title("Server addition")
+    def AddWindowParams(self, WindowGeometry, WindowResizable, WindowTitle):
+        self.__AddIpWindow.geometry(WindowGeometry)
+        self.__AddIpWindow.resizable(WindowResizable, WindowResizable)
+        self.__AddIpWindow.title(WindowTitle)
 
-    labelServerName = tk.Label(Ip_Add_Form_window, text="Server name:")
-    labelServerName.place(x = 10, y = 5, width = 130, height = 20)
+    def AddWindowLabels(self, Master, LabelText, lx, ly, Width, Height):
+        self.Label = tk.Label(Master, text = LabelText)
+        self.Label.place(x = lx, y = ly, width = Width, height = Height)
+    
+    def AddWindowEntry(self, Master, lx, ly, Width, Height, TextVariableName):
+        self.__EntryFieldsTextVariablesDict[TextVariableName] = tk.StringVar()
+        self.EntryBox = tk.Entry(Master, textvariable  = self.__EntryFieldsTextVariablesDict[TextVariableName])
+        self.EntryBox.place(x = lx, y = ly, width = Width, height = Height)
 
-    TextBoxServerName = tk.Entry(Ip_Add_Form_window)
-    TextBoxServerName.place(x = 10, y = 25, width = 130, height = 20)
+    def AddButton(self, Master, Text, ButtonWidth, ButtonHeight, BindFunc, lx, ly, Width, Height):
+        self.Button = tk.Button(Master, text = Text, width = ButtonWidth, height = ButtonHeight, bg="white", fg="black")
+        self.Button.bind("<Button-1>", BindFunc)
+        self.Button.place(x = lx, y = ly, width = Width, height = Height)
 
-    labelServerIP = tk.Label(Ip_Add_Form_window, text="IPv4 Address:")
-    labelServerIP.place(x = 10, y = 44, width = 130, height = 20)
+    def __GetEntryText(self, TextVariable):
+        return self.__EntryFieldsTextVariablesDict[TextVariable].get()
+    
+    def __ClearEntry(self, TextVariable):
+        self.__EntryFieldsTextVariablesDict[TextVariable].set("")    
 
-    TextBoxServerIP = tk.Entry(Ip_Add_Form_window)
-    TextBoxServerIP.place(x = 10, y = 65, width = 130, height = 20)
+    def Initialize(self):
+        self.__AddIpWindow = tk.Tk()
+        self.AddWindowParams("160x115", False, "Server addition")
+        self.AddWindowLabels(self.__AddIpWindow, "Server name:", 10, 5, 130, 20)
+        self.AddWindowLabels(self.__AddIpWindow, "IPv4 Address:", 10, 44, 130, 20)
+        self.AddWindowEntry(self.__AddIpWindow, 10, 25, 130, 20, "ServerName")
+        self.AddWindowEntry(self.__AddIpWindow, 10, 65, 130, 20, "ServerIP")
+        self.AddButton(self.__AddIpWindow, "Add", 10, 2, self.AddServer, 10, 90, 130, 20)
+        self.__AddIpWindow.mainloop()
 
-    BtnAdd = tk.Button(Ip_Add_Form_window, text="Load", width=10, height=2, bg="white", fg="black")
-    BtnAdd.bind("<Button-1>", AddServer)
-    BtnAdd.place(x = 10, y = 90, width = 130, height = 20)
-
-
-    Ip_Add_Form_window.mainloop()
