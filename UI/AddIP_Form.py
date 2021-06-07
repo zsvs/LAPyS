2efd4311fc5f388159733c476f888a2ecdfb6343
ZSVS <stepanezc@gmail.com>
2021-06-07 12:52:55 +0300

    Rewrite to a nested class

--- UI/AddIP_Form.py
+++ UI/AddIP_Form.py
@@ -19,52 +19,27 @@ class AddIpForm(FormBuilder):
 
     def AddServer(self, Event):
         Pool = JSON.Deserialize(Profile.GetProfilePath() +  "\\SERVERS_POOL.json")
-        Pool[self.__GetEntryText("ServerName")] = self.__GetEntryText("ServerIP")
+        Pool[self.GetEntryText("ServerName")] = self.GetEntryText("ServerIP")
         with open(Profile.GetProfilePath() +  "\\SERVERS_POOL.json", "w") as ServerPoolFile:
             ServerPoolFile.write(JSON.Serialize(Pool))
-            Logs.WriteToLog("Added " + self.__GetEntryText("ServerName") + " to servers pool")
-        self.__ClearEntry("ServerName")
-        self.__ClearEntry("ServerIP")
+            Logs.WriteToLog("Added " + self.GetEntryText("ServerName") + " to servers pool")
+        self.ClearEntry("ServerName")
+        self.ClearEntry("ServerIP")
     
     @classmethod
     def GetInstance(cls, Name):
         if cls.__Instance == None:
             cls.__Instance = AddIpForm(Name)
-        return cls.__Instance
-
-    def AddWindowParams(self, WindowGeometry, WindowResizable, WindowTitle):
-        self.__AddIpWindow.geometry(WindowGeometry)
-        self.__AddIpWindow.resizable(WindowResizable, WindowResizable)
-        self.__AddIpWindow.title(WindowTitle)
-
-    def AddWindowLabels(self, Master, LabelText, lx, ly, Width, Height):
-        self.Label = tk.Label(Master, text = LabelText)
-        self.Label.place(x = lx, y = ly, width = Width, height = Height)
-    
-    def AddWindowEntry(self, Master, lx, ly, Width, Height, TextVariableName):
-        self.__EntryFieldsTextVariablesDict[TextVariableName] = tk.StringVar()
-        self.EntryBox = tk.Entry(Master, textvariable  = self.__EntryFieldsTextVariablesDict[TextVariableName])
-        self.EntryBox.place(x = lx, y = ly, width = Width, height = Height)
-
-    def AddButton(self, Master, Text, ButtonWidth, ButtonHeight, BindFunc, lx, ly, Width, Height):
-        self.Button = tk.Button(Master, text = Text, width = ButtonWidth, height = ButtonHeight, bg="white", fg="black")
-        self.Button.bind("<Button-1>", BindFunc)
-        self.Button.place(x = lx, y = ly, width = Width, height = Height)
-
-    def __GetEntryText(self, TextVariable):
-        return self.__EntryFieldsTextVariablesDict[TextVariable].get()
-    
-    def __ClearEntry(self, TextVariable):
-        self.__EntryFieldsTextVariablesDict[TextVariable].set("")    
+        return cls.__Instance 
 
     def Initialize(self):
-        self.__AddIpWindow = tk.Tk()
-        self.AddWindowParams("160x115", False, "Server addition")
-        self.AddWindowLabels(self.__AddIpWindow, "Server name:", 10, 5, 130, 20)
-        self.AddWindowLabels(self.__AddIpWindow, "IPv4 Address:", 10, 44, 130, 20)
-        self.AddWindowEntry(self.__AddIpWindow, 10, 25, 130, 20, "ServerName")
-        self.AddWindowEntry(self.__AddIpWindow, 10, 65, 130, 20, "ServerIP")
-        self.AddButton(self.__AddIpWindow, "Add", 10, 2, self.AddServer, 10, 90, 130, 20)
-        self.__AddIpWindow.mainloop()
+        self.__Window = tk.Tk()
+        self.AddWindowParams(self.__Window, "160x115", False, "Server addition")
+        self.AddWindowLabels(self.__Window, "Server name:", 10, 5, 130, 20)
+        self.AddWindowLabels(self.__Window, "IPv4 Address:", 10, 44, 130, 20)
+        self.AddWindowEntry(self.__Window, 10, 25, 130, 20, "ServerName", None)
+        self.AddWindowEntry(self.__Window, 10, 65, 130, 20, "ServerIP", None)
+        self.AddButton(self.__Window, "Add", 10, 2, self.AddServer, 10, 90, 130, 20)
+        self.__Window.mainloop()
 
 #! print("Hello", AddIpForm.GetInstance().Initialize())
