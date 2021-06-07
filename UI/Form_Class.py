25e8ffaa54b1d8411334dcdead676fb0dcb31225
ZSVS <stepanezc@gmail.com>
2021-06-07 12:53:38 +0300

    Base class for duilding complicated UI forms

--- UI/Form_Class.py
+++ UI/Form_Class.py
@@ -9,8 +9,8 @@ import os
 
 class FormBuilder:
     __EntryFieldsTextVariablesDict = dict()
+    __SubMenuNamesDict = dict()
     __Name = None
-
     def __init__(self, Name):
         __Name = Name
         Logs.WriteToLog("Form {0} created".format(Name))
@@ -23,13 +23,13 @@ class FormBuilder:
         """
         pass
 
-    def AddWindowParams(self, WindowGeometry, WindowResizable, WindowTitle):
+    def AddWindowParams(self, Master, WindowGeometry, WindowResizable, WindowTitle):
         """
         Setting up window params (geometry).
         """
-        self.__AddIpWindow.geometry(WindowGeometry)
-        self.__AddIpWindow.resizable(WindowResizable, WindowResizable)
-        self.__AddIpWindow.title(WindowTitle)
+        Master.geometry(WindowGeometry)
+        Master.resizable(WindowResizable, WindowResizable)
+        Master.title(WindowTitle)
 
     def AddWindowLabels(self, Master, LabelText, lx, ly, Width, Height):
         """
@@ -38,12 +38,13 @@ class FormBuilder:
         self.Label = tk.Label(Master, text = LabelText)
         self.Label.place(x = lx, y = ly, width = Width, height = Height)
     
-    def AddWindowEntry(self, Master, lx, ly, Width, Height, TextVariableName):
+    def AddWindowEntry(self, Master, lx, ly, Width, Height, TextVariableName, ShowParams):
         """
         Add entry field to window.
         """
         self.__EntryFieldsTextVariablesDict[TextVariableName] = tk.StringVar()
         self.EntryBox = tk.Entry(Master, textvariable  = self.__EntryFieldsTextVariablesDict[TextVariableName])
+        self.EntryBox["show"] = ShowParams
         self.EntryBox.place(x = lx, y = ly, width = Width, height = Height)
 
     def AddButton(self, Master, Text, ButtonWidth, ButtonHeight, BindFunc, lx, ly, Width, Height):
@@ -54,31 +55,62 @@ class FormBuilder:
         self.Button.bind("<Button-1>", BindFunc)
         self.Button.place(x = lx, y = ly, width = Width, height = Height)
 
-    def __GetEntryText(self, TextVariable):
+    def AddMenu(self, Master, **kwargs):
+        """
+        Add menu to Master window.
+        You need to adds methods add_command and add_cascade
+        """
+        self.mainMenu = tk.Menu(Master)
+        
+    def AddSubMenu(self, MasterMenu, SubMenuName):
+        """
+        Creating sub menu.
+        Store in __SubMenuNamesDict[SubMenuName] 
+        """
+        self.__SubMenuNamesDict[SubMenuName] = tk.Menu(MasterMenu, tearoff=0)
+        
+    def AddSubMenuCommand(self, SubMenuLabelText, SubMenuCommand, SubMenuName):
+        """
+        Add configs to sub menu
+        """
+        self.__SubMenuNamesDict[SubMenuName].add_command(label = SubMenuLabelText, command = SubMenuCommand)
+
+    def GetSubMenu(self, SubMenuName):
+        """
+        Retrun SubMenu from from SubMenuDict
+        """
+        return self.__SubMenuNamesDict[SubMenuName]
+
+    def GetEntryText(self, TextVariable):
         """
         Return entry TextVariable from dict.
         """
         return self.__EntryFieldsTextVariablesDict[TextVariable].get()
     
-    def __ClearEntry(self, TextVariable):
+    def ClearEntry(self, TextVariable):
         """
         Clear value/text in TextVariable.
         """
         self.__EntryFieldsTextVariablesDict[TextVariable].set("")    
 
+    def SetEntryText(self, TextVariable, Text):
+        """
+        Setting up text to entry with TextVariable
+        """
+        self.__EntryFieldsTextVariablesDict[TextVariable].set(Text)
+
     def Initialize(self):
         """
         Build new window.
         You may sets your params, and add fields that you want.
+        Example:
+        #self.<Your_window_name> = tk.Tk()
+        #self.AddWindowParams(<Your_window_name>, "160x115", False, "Server addition")
+        #self.AddWindowLabels(<Your_window_name>, "Server name:", 10, 5, 130, 20)
+        #self.AddWindowLabels(<Your_window_name>, "IPv4 Address:", 10, 44, 130, 20)
+        #self.AddWindowEntry(<Your_window_name>, 10, 25, 130, 20, "ServerName")
+        #self.AddWindowEntry(<Your_window_name>, 10, 65, 130, 20, "ServerIP")
+        #self.AddButton(<Your_window_name>, "Add", 10, 2, self.AddServer, 10, 90, 130, 20)
+        #self.<Your_window_name>.mainloop()
         """
-        self.__AddIpWindow = tk.Tk()
-
-        ###!Example!###
-        #self.AddWindowParams("160x115", False, "Server addition")
-        #self.AddWindowLabels(self.__AddIpWindow, "Server name:", 10, 5, 130, 20)
-        #self.AddWindowLabels(self.__AddIpWindow, "IPv4 Address:", 10, 44, 130, 20)
-        #self.AddWindowEntry(self.__AddIpWindow, 10, 25, 130, 20, "ServerName")
-        #self.AddWindowEntry(self.__AddIpWindow, 10, 65, 130, 20, "ServerIP")
-        #self.AddButton(self.__AddIpWindow, "Add", 10, 2, self.AddServer, 10, 90, 130, 20)
-
-        self.__AddIpWindow.mainloop()
+        pass
